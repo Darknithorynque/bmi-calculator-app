fee4143f3d70d792ff4b5d075b79ce9dd32d4843
Birkan Arda <birkan.malkoc@vagani.com>
2023-07-27 23:55:53 +0300

    changed basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/bmi-calculator">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App4 />
     </BrowserRouter>
   </React.StrictMode>
